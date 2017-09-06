f5145fe16576d15cd6812135bf67da2097414fe3
Dario Caruso <dev@dariocaruso.info>
2017-09-07 00:11:06 +0200

    trying with new sleep

--- configure-node.sh
+++ configure-node.sh
@@ -26,15 +26,52 @@ curl -i -u $DB_USER:$DB_PASSW -X POST http://127.0.0.1:8091/settings/indexes -d 
 
 echo "Type: $TYPE"
 
+
+
 IP=$(ifconfig | grep $OVERLAYNET | sed 's/:/ /g' |awk '{print($3)}')
 
-sleeptime0=$(( ( RANDOM % 10 )  + 1 ))
-sleeptime=$(( $(( ( RANDOM % 10 )  + 10 )) * $(( ( RANDOM % 10 )  + 1 )) ))
+
+
+
+
+if [ $TYPE = 'WORKER' ]; then
+
+sleeptime=$(( $(( ( RANDOM % 10 )  + 6 )) * $(( ( RANDOM % 10 )  + 6 )) ))
 
 echo "Sleeping ...$sleeptime"
 sleep $sleeptime
 
 
+
+
+    if curl -u $DB_USER:$DB_PASSW --output /dev/null --silent --head --fail "http://$COUCHBASE_MASTER:8091/pools/default"; then
+        
+        curl -u $DB_USER:$DB_PASSW -d otpNode=ns_1@$IP http://$COUCHBASE_MASTER:8091/controller/failOver
+        
+        
+        
+        sleep 10
+        
+        curl -u $DB_USER:$DB_PASSW -d otpNode=ns_1@$IP http://$COUCHBASE_MASTER:8091/controller/ejectNode
+        
+        
+        
+        
+        
+        sleep 10
+        
+        couchbase-cli rebalance --cluster="$COUCHBASE_MASTER:8091" --user="$DB_USER" --password="$DB_PASSW" --server-add="$IP" --server-add-username="$DB_USER" --server-add-password="$DB_PASSW"
+        
+        
+        
+        fg 1
+        exit
+        
+        
+    fi
+    
+    
+fi
 
 
 ipslit=$(echo $IP | sed 's/\./ /g')
@@ -45,6 +82,8 @@ OVERLAYNET="$(echo $ipslit| awk '{print($1)}').$(echo $ipslit| awk '{print($2)}'
 all=$(nmap -sn $OVERLAYNET.0/24  -oG - | grep Host | grep $OVERLAYNET | grep -v "$IP" | grep -v $OVERLAYNET.1| awk '{print($2)}')
 
 echo $all
+
+
 
 for i in $all; do
     echo $i
