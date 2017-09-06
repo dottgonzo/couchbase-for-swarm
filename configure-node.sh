50757d5245d62ce36d857909b66e8b1d94f1421e
dario <darioyzf@gmail.com>
2017-09-06 17:29:43 +0200

    reup

--- configure-node.sh
+++ configure-node.sh
@@ -38,22 +38,7 @@ if [ "$TYPE" == "WORKER" ]; then
   echo "Sleeping ..."
   sleep 60
 
-  #IP=`hostname -s`
-  echo "IP: " $IP
-
-
-curl -u $DB_USER:$DB_PASSW -d otpNode=ns_1@$IP http://$COUCHBASE_MASTER:8091/controller/failOver
-
-sleep 5
-
-  echo "Auto Rebalance: $AUTO_REBALANCE"
-  if [ "$AUTO_REBALANCE" = "true" ]; then
-    couchbase-cli rebalance --cluster=$COUCHBASE_MASTER:8091 --user=$DB_USER --password=$DB_PASSW --server-add=$IP --server-add-username=$DB_USER --server-add-password=$DB_PASSW
-  else
-    couchbase-cli server-add --cluster=$COUCHBASE_MASTER:8091 --user=$DB_USER --password=$DB_PASSW --server-add=$IP --server-add-username=$DB_USER --server-add-password=$DB_PASSW
-  fi;
-
-else
+fi
 
 
 
@@ -102,6 +87,5 @@ sleep 1
 done
 
 
-fi;
 
 fg 1
