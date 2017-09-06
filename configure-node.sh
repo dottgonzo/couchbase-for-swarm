8058b17dc28493c75fc8d19c06a0580e4f201715
Dario Caruso <dev@dariocaruso.info>
2017-09-06 21:20:07 +0200

    echo all

--- configure-node.sh
+++ configure-node.sh
@@ -43,6 +43,8 @@ OVERLAYNET="$(echo $ipslit| awk '{print($1)}').$(echo $ipslit| awk '{print($2)}'
 
 all=$(nmap -sn $OVERLAYNET.0/24  -oG - | grep $OVERLAYNET | grep -v "$IP" | grep -v $OVERLAYNET.1)
 
+echo $all
+
 for i in $all; do
     
     curl -u $DB_USER:$DB_PASSW -d otpNode=ns_1@$IP http://$i:8091/pool/default
